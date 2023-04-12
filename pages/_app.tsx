7dc1e49dddd192b03d93087fa070ee5efe56bc4d
Elia <mkumboelia@gmail.com>
2023-04-12 19:03:13 +0300

    added tracking for the website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Script id="m-clarity" type='text/javascript' strategy="afterInteractive"
+      dangerouslySetInnerHTML={
+        {
+          __html:
+            `
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "gngfcg14ur");
+        `
+        }
+      } />
+    <Component {...pageProps} />
+  </>
 }
