faa8852ed79dd3447394c1e4022af5b454298e24
Elia <mkumboelia@gmail.com>
2023-08-21 12:43:07 +0300

    fix: updated bot id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Script from 'next/script'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Script id="m-clarity" type='text/javascript' strategy="afterInteractive"
-      dangerouslySetInnerHTML={
-        {
-          __html:
-            `
+  return (
+    <>
+      <Script
+        id="m-clarity"
+        type="text/javascript"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
           (function(c,l,a,r,i,t,y){
             c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "gngfcg14ur");
-        `
-        }
-      } />
-    <script
-      id={'chatbox'}
-      dangerouslySetInnerHTML={{
-        __html: `document.addEventListener('DOMContentLoaded', function() {
+        `,
+        }}
+      />
+      <script
+        id={"chatbox"}
+        dangerouslySetInnerHTML={{
+          __html: `document.addEventListener('DOMContentLoaded', function() {
           // setting global variables
-          window.botId = 1078
+          window.botId = 1258
           
           // create div with id = sarufi-chatbox
           const div = document.createElement("div")
@@ -43,8 +45,9 @@ export default function App({ Component, pageProps }: AppProps) {
           style.href = "https://cdn.jsdelivr.net/gh/flexcodelabs/sarufi-chatbox/example/vanilla-js/style.css"
           document.head.appendChild(style)
         });`,
-      }}
-    />
-    <Component {...pageProps} />
-  </>
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
